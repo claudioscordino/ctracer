385d4dc70b6c57ab32db6c1ba28c9a96f16f309a
Claudio Scordino <claudio@evidence.eu.com>
2019-05-08 15:35:44 +0200

    Attributes for enable/disable functions; better naming for variables.

--- ctracer.c
+++ ctracer.c
@@ -4,37 +4,24 @@
 #include <stdint.h>
 #include <time.h>
 
-#define	LOG_SIZE	1024
-#define	EV_START	0x1
-#define	EV_END		0x2
+#include "ctracer.h"
 
-struct event {
-	uint8_t 	type;
-	uint64_t	ip;
-	uint64_t	pip;
-	uint64_t	arg1;
-	uint64_t	arg2;
-	uint64_t	arg3;
-	uint64_t	arg4;
-	uint64_t	time;
-};
-
-struct event data [LOG_SIZE];
-uint16_t curr = 0;
+struct event trace [LOG_SIZE];
+static uint16_t curr = 0;
 
 __attribute__ ((no_instrument_function))
 void trace_function(uint64_t ip, uint64_t parent_ip, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4)
 {
 	struct timespec ts;
 	clock_gettime(CLOCK_MONOTONIC, &ts);
-	data[curr].type = EV_START;
-	data[curr].ip = ip;
-	data[curr].pip = parent_ip;
-	data[curr].arg1	 = arg1;
-	data[curr].arg2	 = arg2;
-	data[curr].arg3	 = arg3;
-	data[curr].arg4	 = arg4;
-	data[curr].time = (ts.tv_sec * 1000000000) + ts.tv_nsec;
+	trace[curr].type = EV_START;
+	trace[curr].ip = ip;
+	trace[curr].pip = parent_ip;
+	trace[curr].arg1	 = arg1;
+	trace[curr].arg2	 = arg2;
+	trace[curr].arg3	 = arg3;
+	trace[curr].arg4	 = arg4;
+	trace[curr].time = (ts.tv_sec * 1000000000) + ts.tv_nsec;
 	curr=(curr+1)%LOG_SIZE;
 }
 
@@ -47,32 +34,31 @@ void trace_dump()
 	fprintf(fd, "Event:\tIP:\tPIP:\targ1:\targ2:\targ3:\targ4:\ttime:\n");
 	fprintf(fd, "==============================================================\n");
 	for (int i = 0; i < LOG_SIZE; ++i) {
-		if (data[i].type == 0)
+		if (trace[i].type == 0)
 			break;
 		fprintf(fd, "%u\t0x%lx\t0x%lx\t0x%lx\t0x%lx\t0x%lx\t0x%lx\t%lu\n",
-			data[i].type,
-			data[i].ip,
-			data[i].pip,
-			data[i].arg1,
-			data[i].arg2,
-			data[i].arg3,
-			data[i].arg4,
-			data[i].time);
+			trace[i].type,
+			trace[i].ip,
+			trace[i].pip,
+			trace[i].arg1,
+			trace[i].arg2,
+			trace[i].arg3,
+			trace[i].arg4,
+			trace[i].time);
 	}
 	fclose(fd);
 }
 
-
-typedef void (*ctracer_func_t)(uint64_t ip, uint64_t parent_ip, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4);
-
 // Set equal to &trace_function from tracing from beginning (e.g. main function)
 ctracer_func_t ctracer_ptr __attribute__((__section__(".data.read_mostly"))) = NULL;
 
+__attribute__ ((no_instrument_function))
 void ctracer_enable(void)
 {
 	ctracer_ptr = &trace_function;
 }
 
+__attribute__ ((no_instrument_function))
 void ctracer_disable(void)
 {
 	ctracer_ptr = NULL;
--- ctracer.h
+++ ctracer.h
@@ -1,6 +1,23 @@
 #ifndef	CTRACER_H
 #define	CTRACER_H
 
+#define	LOG_SIZE	1024
+#define	EV_START	0x1
+#define	EV_END		0x2
+
+struct event {
+	uint8_t 	type;
+	uint64_t	ip;
+	uint64_t	pip;
+	uint64_t	arg1;
+	uint64_t	arg2;
+	uint64_t	arg3;
+	uint64_t	arg4;
+	uint64_t	time;
+};
+
+typedef void (*ctracer_func_t)(uint64_t ip, uint64_t parent_ip, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4);
+
 extern void ctracer_enable(void);
 extern void ctracer_disable(void);
 extern void trace_dump();
